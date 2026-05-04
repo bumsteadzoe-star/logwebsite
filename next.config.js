14c03f7a9b333ff465fd6e2ed68149aad9ade26f
Avery Voss <agvoss12@gmail.com>
2026-05-03 17:24:03 -0700

    Add back well-known

--- next.config.js
+++ next.config.js
@@ -4,5 +4,13 @@ const nextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [{ key: 'Content-Type', value: 'application/json' }],
+      },
+    ]
+  },
 }
 module.exports = nextConfig
